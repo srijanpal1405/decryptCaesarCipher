048e5fb583f0460457feec3f6000e8867e1deb19
Srijan <srijanpal1405@gmail.com>
2023-04-05 17:18:40 +0530

    comment

--- findKey.py
+++ findKey.py
@@ -11,7 +11,7 @@ for i in range(1,26):
     strfile="".join(fileContent)
     all_conf.append(str(detect_langs(strfile)[0]))
     file2.close()
-
+# print(all_conf)
 for i in range(0,25):
     if float(all_conf[i][3:]) > 0.9:
         shift_li.append(all_conf[i][0:3]+str(i+1))
